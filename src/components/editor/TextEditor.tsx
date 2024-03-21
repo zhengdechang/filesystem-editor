adb44056419299cb28f11d63e020e09112831303
zhengdechang <zhengdechang@jancsitect.net>
2024-03-21 15:51:43 +0800

    add aieditor

--- src/components/editor/TextEditor.tsx
+++ src/components/editor/TextEditor.tsx
@@ -1,13 +1,39 @@
 import React, { useEffect, useRef } from "react";
 import { observer } from "mobx-react";
-import { EditorView, keymap, highlightSpecialChars, drawSelection, dropCursor, useCodeMirror } from "@uiw/react-codemirror";
-
-import { autocompletion, closeBrackets, closeBracketsKeymap, completionKeymap } from "@codemirror/autocomplete";
+import {
+  EditorView,
+  keymap,
+  highlightSpecialChars,
+  drawSelection,
+  dropCursor,
+  useCodeMirror,
+} from "@uiw/react-codemirror";
+import { AiEditor } from "aieditor";
+import "aieditor/dist/style.css";
+import {
+  autocompletion,
+  closeBrackets,
+  closeBracketsKeymap,
+  completionKeymap,
+} from "@codemirror/autocomplete";
 import { defaultKeymap, history, historyKeymap } from "@codemirror/commands";
-import { bracketMatching, defaultHighlightStyle, foldGutter, foldKeymap, indentOnInput, indentService, indentUnit, syntaxHighlighting } from "@codemirror/language";
+import {
+  bracketMatching,
+  defaultHighlightStyle,
+  foldGutter,
+  foldKeymap,
+  indentOnInput,
+  indentService,
+  indentUnit,
+  syntaxHighlighting,
+} from "@codemirror/language";
 import { searchKeymap, highlightSelectionMatches } from "@codemirror/search";
 import { EditorState, Extension } from "@codemirror/state"; // This package isn't listed in package.json because it then conflicts with @uiw/react-codemirror for some reason. It's included through that package instead. Can try installing it as a first-class package if either of them get updates later.
-import { highlightActiveLineGutter, lineNumbers, rectangularSelection } from "@codemirror/view";
+import {
+  highlightActiveLineGutter,
+  lineNumbers,
+  rectangularSelection,
+} from "@codemirror/view";
 
 import { languages } from "@codemirror/language-data";
 import { inform7 } from "codemirror-lang-inform7";
@@ -24,194 +50,252 @@ import { isSnippetsVariant } from "services/app/env";
 const TAB_SIZE = 4;
 
 interface TextEditorElementProps {
-    language?: string;
-    onChange: ( newValue: string ) => void;
-    options: EditorOptions;
-    value: string;
+  language?: string;
+  onChange: (newValue: string) => void;
+  options: EditorOptions;
+  value: string;
 }
 
 interface EditorOptions {
-    editable: boolean;
-    fontFamily: string;
-    fontSize: number;
-    lineNumbers: boolean;
-    syntaxHighlighting: boolean;
-    tabChars: string;
-    wordWrap: boolean;
-    wrappingIndent: boolean;
+  editable: boolean;
+  fontFamily: string;
+  fontSize: number;
+  lineNumbers: boolean;
+  syntaxHighlighting: boolean;
+  tabChars: string;
+  wordWrap: boolean;
+  wrappingIndent: boolean;
 }
 
 // These are exported so that we can use them in the Storybook stories
 export const defaultOptions: EditorOptions = {
-    editable: true,
-    fontFamily: "monospace",
-    fontSize: 15,
-    lineNumbers: true,
-    syntaxHighlighting: true,
-    tabChars: "    ",
-    wordWrap: true,
-    wrappingIndent: true
+  editable: true,
+  fontFamily: "monospace",
+  fontSize: 15,
+  lineNumbers: true,
+  syntaxHighlighting: true,
+  tabChars: "    ",
+  wordWrap: true,
+  wrappingIndent: true,
 };
 
 /**
  * The code editor itself. We're using CodeMirror 6, see https://codemirror.net/6/docs/ and https://github.com/uiwjs/react-codemirror/
  */
-export const TextEditorElement: React.FC<TextEditorElementProps> = ({ onChange, options, value }) => {
-    let extensions: Extension[] = [
-        autocompletion(),
-        bracketMatching(),
-        closeBrackets(),
-        syntaxHighlighting( defaultHighlightStyle, { fallback: true }),
-        drawSelection(),
-        dropCursor(),
-        EditorState.allowMultipleSelections.of( true ),
-        EditorState.tabSize.of( TAB_SIZE ),
-        highlightSelectionMatches(),
-        highlightSpecialChars(),
-        history(),
-        indentOnInput(),
-        rectangularSelection(),
-        keymap.of( [
-            ...closeBracketsKeymap,
-            ...completionKeymap,
-            ...defaultKeymap,
-            ...foldKeymap,
-            ...historyKeymap,
-            ...searchKeymap
-        ] )
+export const TextEditorElement: React.FC<TextEditorElementProps> = ({
+  onChange,
+  options,
+  value,
+}) => {
+  let extensions: Extension[] = [
+    autocompletion(),
+    bracketMatching(),
+    closeBrackets(),
+    syntaxHighlighting(defaultHighlightStyle, { fallback: true }),
+    drawSelection(),
+    dropCursor(),
+    EditorState.allowMultipleSelections.of(true),
+    EditorState.tabSize.of(TAB_SIZE),
+    highlightSelectionMatches(),
+    highlightSpecialChars(),
+    history(),
+    indentOnInput(),
+    rectangularSelection(),
+    keymap.of([
+      ...closeBracketsKeymap,
+      ...completionKeymap,
+      ...defaultKeymap,
+      ...foldKeymap,
+      ...historyKeymap,
+      ...searchKeymap,
+    ]),
+  ];
+
+  // Set code highlighting to the detected language.
+  if (options.syntaxHighlighting) {
+    switch (editorStateStore.language) {
+      case "css":
+        extensions.push(css());
+        break;
+
+      case "inform7":
+        extensions.push(inform7());
+        break;
+
+      case "javascript":
+        extensions.push(javascript());
+        break;
+
+      case "json":
+        extensions.push(json());
+        break;
+
+      case "markdown":
+        extensions.push(
+          markdown({ base: markdownLanguage, codeLanguages: languages })
+        );
+        break;
+    }
+  }
+
+  // set the indent characters to tab or spaces
+  extensions.push(indentUnit.of(options.tabChars));
+
+  // indents the next line based on how much the previous one was indented
+  const autoIndentExtension = indentService.of((context, pos) => {
+    const previousLine = context.lineAt(pos, -1);
+
+    // if the previous line has no text other than whitespace, don't indent the next line
+    if (previousLine.text.trim().length === 0) {
+      return 0;
+    }
+
+    const previousIndentChars = previousLine.text.match(
+      new RegExp(`^(${options.tabChars})*`)
+    );
+    const multiplier = options.tabChars[0] === "\t" ? TAB_SIZE : 1; // when using tabs for indentation, must multiply the result by tab length for the editor to insert tab characters
+
+    return previousIndentChars
+      ? previousIndentChars[0]?.length * multiplier
+      : 0;
+  });
+  extensions.push(autoIndentExtension);
+
+  if (options.lineNumbers) {
+    // line numbers option enables or disables the entire gutter
+    extensions = [
+      ...extensions,
+      lineNumbers(),
+      highlightActiveLineGutter(),
+      foldGutter(),
     ];
+  }
 
-    // Set code highlighting to the detected language.
-    if( options.syntaxHighlighting ) {
-        switch( editorStateStore.language ) {
-            case "css":
-                extensions.push( css() );
-                break;
+  if (options.wordWrap) {
+    extensions.push(EditorView.lineWrapping);
+  }
 
-            case "inform7":
-                extensions.push( inform7() );
-                break;
+  const theme = EditorView.theme({
+    ".cm-content": {
+      fontFamily: options.fontFamily,
+      paddingLeft: options.wrappingIndent ? "1.5em" : "0",
+      textIndent: options.wrappingIndent ? "-1.5em" : "0",
+    },
+    ".cm-content, .cm-gutters": {
+      fontSize: options.fontSize + "px",
+    },
+  });
 
-            case "javascript":
-                extensions.push( javascript() );
-                break;
-
-            case "json":
-                extensions.push( json() );
-                break;
-
-            case "markdown":
-                extensions.push( markdown({ base: markdownLanguage, codeLanguages: languages }) );
-                break;
-        }
-    }
-
-    // set the indent characters to tab or spaces
-    extensions.push( indentUnit.of( options.tabChars ) );
-
-    // indents the next line based on how much the previous one was indented
-    const autoIndentExtension = indentService.of( ( context, pos ) => {
-        const previousLine = context.lineAt( pos, -1 );
-
-        // if the previous line has no text other than whitespace, don't indent the next line
-        if( previousLine.text.trim().length === 0 ) {
-            return 0;
-        }
-
-        const previousIndentChars = previousLine.text.match( new RegExp( `^(${options.tabChars})*` ) );
-        const multiplier = ( options.tabChars[0] === "\t" ) ? TAB_SIZE : 1;   // when using tabs for indentation, must multiply the result by tab length for the editor to insert tab characters
-
-        return previousIndentChars ? previousIndentChars[0]?.length * multiplier : 0;
-    });
-    extensions.push( autoIndentExtension );
-
-    if( options.lineNumbers ) {
-        // line numbers option enables or disables the entire gutter
-        extensions = [
-            ...extensions,
-            lineNumbers(),
-            highlightActiveLineGutter(),
-            foldGutter()
-        ];
-    }
-
-    if( options.wordWrap ) {
-        extensions.push( EditorView.lineWrapping );
-    }
-
-    const theme = EditorView.theme({
-        ".cm-content": {
-            fontFamily: options.fontFamily,
-            paddingLeft: options.wrappingIndent ? "1.5em" : "0",
-            textIndent: options.wrappingIndent ? "-1.5em" : "0"
+  const editorRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (editorRef.current) {
+      const aiEditor = new AiEditor({
+        element: editorRef.current,
+        placeholder: "点击输入内容...",
+        content: value,
+        onChange: (aiEditor: any) => {
+          // 监听到用编辑器内容发生变化了，控制台打印编辑器的 html 内容...
+          console.log("object");
+          onChange(aiEditor.getMarkdown());
         },
-        ".cm-content, .cm-gutters": {
-            fontSize: options.fontSize + "px"
-        }
-    });
+        ai: {
+          models: {
+            spark: {
+              protocol: "wss",
+              appId: "8cf39191",
+              apiKey: "3640caaedbbe57f407d4d33b33567c99",
+              apiSecret: "NjJhZjdhM2VkYzYxMzNkYjIwOGFkMzYw",
+              version: "v3.1",
+            },
+          },
+          bubblePanelEnable: true,
+          bubblePanelModel: "spark",
+        },
+      });
+      return () => {
+        aiEditor.destroy();
+      };
+    }
+  }, []);
+  // const { setContainer, view } = useCodeMirror({
+  //   autoFocus: true,
+  //   basicSetup: false,
+  //   container: editorRef.current,
+  //   editable: options.editable,
+  //   extensions,
+  //   onChange,
+  //   theme,
+  //   value,
+  // });
 
-    const editorRef = useRef<HTMLDivElement>( null );
-    const { setContainer, view } = useCodeMirror({
-        autoFocus: true,
-        basicSetup: false,
-        container: editorRef.current,
-        editable: options.editable,
-        extensions,
-        onChange,
-        theme,
-        value
-    });
+  // useEffect(() => {
+  //   if (editorRef.current) {
+  //     setContainer(editorRef.current);
+  //   }
+  // }, [editorRef.current]);
 
-    useEffect( () => {
-        if( editorRef.current ) {
-            setContainer( editorRef.current );
-        }
-    }, [ editorRef.current ] );
+  // useEffect(() => {
+  //   editorStateStore.setEditorReference(view);
+  // }, [view]);
 
-    useEffect( () => {
-        editorStateStore.setEditorReference( view );
-    }, [ view ] );
-
-    return <div ref={editorRef} className="editor-container" />;
+  return (
+    <div
+      ref={editorRef}
+      className="editor-container"
+      style={{ height: "100%" }}
+    />
+  );
 };
 
-const TextEditor: React.FC = observer( () => {
-    const { contents, language } = editorStateStore;
+const TextEditor: React.FC = observer(() => {
+  const { contents, language } = editorStateStore;
 
-    // when text is entered, send it to the state store which handles passing
-    // it to other components that need it, and saving it
-    const onChange = ( newValue: string ): void => {
-        editorStateStore.setContents( newValue, false );
-    };
+  // when text is entered, send it to the state store which handles passing
+  // it to other components that need it, and saving it
+  const onChange = (newValue: string): void => {
+    editorStateStore.setContents(newValue, false);
+  };
 
-    // editing is disabled only in Snippets for all files that aren't the main file
-    const editable = !( isSnippetsVariant && projectStore.entryFile?.id !== editorStateStore.file?.id );
-    const fontFamily = ( settingsStore.getSetting( "editor", "fontFamily" ) === "sans-serif" )
-        ? "Lato, Arial, sans-serif"
-        : "Menlo, Monaco, \"Courier New\", monospace";
-    const fontSize = settingsStore.getSetting( "editor", "fontSize" );
-    const lineNumbers = settingsStore.getSetting( "editor", "lineNumbers" );
-    const syntaxHighlighting = settingsStore.getSetting( "language", "syntaxHighlighting", true );
-    const tabChars = projectStore.manager.tabIndent ? "\t" : "    ";
-    const wordWrap = settingsStore.getSetting( "editor", "wordWrap" );
-    const wrappingIndent = settingsStore.getSetting( "editor", "wrappingIndent" );
+  // editing is disabled only in Snippets for all files that aren't the main file
+  const editable = !(
+    isSnippetsVariant &&
+    projectStore.entryFile?.id !== editorStateStore.file?.id
+  );
+  const fontFamily =
+    settingsStore.getSetting("editor", "fontFamily") === "sans-serif"
+      ? "Lato, Arial, sans-serif"
+      : 'Menlo, Monaco, "Courier New", monospace';
+  const fontSize = settingsStore.getSetting("editor", "fontSize");
+  const lineNumbers = settingsStore.getSetting("editor", "lineNumbers");
+  const syntaxHighlighting = settingsStore.getSetting(
+    "language",
+    "syntaxHighlighting",
+    true
+  );
+  const tabChars = projectStore.manager.tabIndent ? "\t" : "    ";
+  const wordWrap = settingsStore.getSetting("editor", "wordWrap");
+  const wrappingIndent = settingsStore.getSetting("editor", "wrappingIndent");
 
-    const options: EditorOptions = {
-        ...defaultOptions,
-        editable,
-        fontFamily,
-        fontSize,
-        lineNumbers,
-        syntaxHighlighting,
-        tabChars,
-        wordWrap,
-        wrappingIndent
-    };
+  const options: EditorOptions = {
+    ...defaultOptions,
+    editable,
+    fontFamily,
+    fontSize,
+    lineNumbers,
+    syntaxHighlighting,
+    tabChars,
+    wordWrap,
+    wrappingIndent,
+  };
 
-    return <TextEditorElement language={language}
-                              value={contents}
-                              options={options}
-                              onChange={onChange} />;
+  return (
+    <TextEditorElement
+      language={language}
+      value={contents}
+      options={options}
+      onChange={onChange}
+    />
+  );
 });
 
 export default TextEditor;
--- src/components/layout/IDE.tsx
+++ src/components/layout/IDE.tsx
@@ -1,3 +1,8 @@
+/*
+ * @Description:
+ * @Author: Devin
+ * @Date: 2024-03-19 10:06:32
+ */
 import React from "react";
 
 import { observer } from "mobx-react";
@@ -12,33 +17,40 @@ import { isSnippetsVariant } from "services/app/env";
 import "./IDE.scss";
 
 const FileManager = isSnippetsVariant
-    ? require( "components/fileManager/ReadOnlyFileManager" ).default
-    : require( "components/fileManager/FileManager" ).default;
+  ? require("components/fileManager/ReadOnlyFileManager").default
+  : require("components/fileManager/FileManager").default;
 
 /**
  * This is the main component for the entire IDE view.
  */
-const IDE: React.FC = observer( () => {
-    return <div id="ide">
-        <MainNavigation />
-        <div id="workspace" className={ideStateStore.fileManagerOpen ? "is-file-manager-open" : undefined}>
-            {ideStateStore.fileManagerOpen && <FileManager />}
-            <Split minSize={300}
-                   cursor="col-resize">
-                {({
-                    getGridProps,
-                    getGutterProps
-                // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                }: any ): JSX.Element => (
-                    <div className="grid" {...getGridProps()}>
-                        <Pane key="left" side="left" />
-                        <div className="gutter-col is-hidden-mobile" {...getGutterProps( "column", 1 )} />
-                        <Pane key="right" side="right" />
-                    </div>
-                )}
-            </Split>
-        </div>
-    </div>;
+const IDE: React.FC = observer(() => {
+  return (
+    <div id="ide">
+      <MainNavigation />
+      <div
+        id="workspace"
+        className={
+          ideStateStore.fileManagerOpen ? "is-file-manager-open" : undefined
+        }
+      >
+        {ideStateStore.fileManagerOpen && <FileManager />}
+        <Split minSize={300} cursor="col-resize">
+          {({
+            getGridProps,
+            getGutterProps,
+          }: // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          any): JSX.Element => (
+            //   <div className="grid" {...getGridProps()}>
+            <div style={{ width: "100%" }}>
+              <Pane key="left" side="left" />
+              {/* <div className="gutter-col is-hidden-mobile" {...getGutterProps( "column", 1 )} />
+                        <Pane key="right" side="right" /> */}
+            </div>
+          )}
+        </Split>
+      </div>
+    </div>
+  );
 });
 
 export default IDE;
